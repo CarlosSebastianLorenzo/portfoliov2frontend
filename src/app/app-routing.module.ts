3c8d793291b6c42e9ac65c43fc7c2f1385f1fc6f
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-08-31 01:14:02 -0300

    Componentes creados+pagina404+routinghecho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {path: 'portfolio', component:PortfolioComponent},
+  {path: 'portfolio/:id', component:PortfolioComponent},
+  {path: 'acercade', component:PaginaNoEncontradaComponent},
+  {path: '', redirectTo: 'portfolio', pathMatch: 'full'},
+  {path: '**', redirectTo: 'pagina-no-encontrada', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    PaginaNoEncontradaComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    AcercaDeComponent,
   ],
   imports: [
     BrowserModule,

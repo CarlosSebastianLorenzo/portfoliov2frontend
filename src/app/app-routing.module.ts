8a287637c2b30101e9b16cbffb8b63d26f4abce8
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-09-04 11:34:00 -0300

    SeAgregaronLasRutasDeLogin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
 import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
-
+import { LoginComponent } from './componentes/login/login.component';
 
 
 
@@ -11,6 +11,7 @@ const routes: Routes = [
   {path: 'portfolio/:id', component:PortfolioComponent},
   {path: 'acercade', component:PaginaNoEncontradaComponent},
   {path: '', redirectTo: 'portfolio', pathMatch: 'full'},
+  {path: 'login', component:LoginComponent},
   {path: '**', redirectTo: 'pagina-no-encontrada', pathMatch: 'full'}
 ];
 

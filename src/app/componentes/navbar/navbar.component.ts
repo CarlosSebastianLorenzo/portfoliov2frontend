541ff5454f5f15bb7a4e31f41e7f12ebd14378ac
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-10-03 15:51:39 -0300

    navbar href y habilidades width ngStyle

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  ruta: string;
+  constructor(private router : Router) {
+    this.ruta = "#";
+   }
 
   ngOnInit(): void {
+    this.ruta = this.router.url;
   }
 
 }

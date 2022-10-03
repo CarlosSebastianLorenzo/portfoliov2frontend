e41c1cd92b80c0cb2816fc5ea21e51f0a86c556f
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-10-02 23:49:19 -0300

    Experiencia Modelo

---
+++ src/app/Modelos/experiencia.model.ts
@@ -0,0 +1,10 @@
+export interface Experiencias {
+    "id" : number,
+    "empresa" : string,
+    "puesto" : string,
+    "fechaInicio" : Date,
+    "fechaFinal" : Date,
+    "logo" : string,
+    "actividades" : string,
+    "usuario" : { "id" : number }
+}

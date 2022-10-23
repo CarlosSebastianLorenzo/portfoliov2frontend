8fadeb0c1d64aca25128e89058077be606291599
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-10-23 13:04:18 -0300

    Experiencia Modal Agregar y Cambiar botón

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ExperienciaService } from 'src/app/servicios/experiencia.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Experiencias } from 'src/app/Modelos/experiencia.model';
+import { AutenticacionService } from '../../servicios/autenticacion.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-experiencia',
@@ -11,10 +14,16 @@ import { Experiencias } from 'src/app/Modelos/experiencia.model';
 export class ExperienciaComponent implements OnInit {
   misexperiencias:any;
   public exp: Experiencias;
+  experienciasmodal: any;
   idusuario: number = 0;
   protocolo: string = '';
+  formulario:FormGroup;
+  isLogged: boolean = false;
 
-  constructor(private experiencias: ExperienciaService, private route: ActivatedRoute) {
+  constructor(private formBuilder: FormBuilder,
+              private experiencias: ExperienciaService,
+              private route: ActivatedRoute,
+              private autenticacionService:AutenticacionService) {
     this.exp ={
       id: 1,
       empresa : '',
@@ -24,14 +33,39 @@ export class ExperienciaComponent implements OnInit {
       logo : '',
       actividades : '',
       usuario : { id : 1 }
-    }
+    };
+    this.formulario=this.formBuilder.group({
+      id:[''],
+      empresa:['',[Validators.required]],
+      puesto:['',[Validators.required]],
+      fechaInicio:[''],
+      fechaFinal:[''],
+      logo:[''],
+      actividades: ['']
+    })
    }
 
   ngOnInit(): void {
     this.experiencias.obtenerDatos().subscribe((data)=>{
       this.misexperiencias = data.experiencia;
     });
+    //Verifica si la ruta coincide con el usuario logueado y muestra los botones
+    this.route.paramMap.subscribe((ruta: ParamMap) => {
+      this.idusuario = parseInt(ruta.get('ruta')!);
+      var currentUser=this.autenticacionService.UsuarioAutenticado;
+      if (currentUser && currentUser.id==this.idusuario){
+        this.isLogged=true;
+      }
+    });
   }
+
+  refresh(milisegundos:number) {
+    setTimeout(() => {
+       this.experiencias.obtenerDatos().subscribe((data)=>{
+        this.misexperiencias = data.experiencia;
+      });
+    }, milisegundos);
+}
 
   onDelete(id: number) {
     
@@ -39,37 +73,55 @@ export class ExperienciaComponent implements OnInit {
 
     this.experiencias.borrarDato(id).subscribe((data) => {});
 
-    // refresh
-    this.experiencias.obtenerDatos().subscribe((data)=>{
-      this.misexperiencias = data.experiencia;
-    });
+    this.refresh(500);
   }
 
-  onChange(id: number) {
+  openModal(experiencias:Experiencias){
+    this.experienciasmodal =[experiencias];
+    setTimeout(() => {
+      var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+      myModal.show();
+    }, 50);
+  }  
+
+  onChange(event: Event) {
+    event.preventDefault;
     
     this.route.paramMap.subscribe((ruta: ParamMap) => {
       this.idusuario = parseInt(ruta.get('ruta')!);
 
       this.exp = {
-      id: id,
-      empresa : 'Nasa',
-      puesto : 'Dueño',
-      fechaInicio : new Date(2022,10,2),
-      fechaFinal : new Date(2022,10,2),
-      logo : '123123',
-      actividades : 'Jefe',
+      id: parseInt(this.formulario.value.id),
+      empresa : this.formulario.value.empresa,
+      puesto : this.formulario.value.puesto,
+      fechaInicio : this.formulario.value.fechaInicio,
+      fechaFinal : this.formulario.value.fechaFinal,
+      logo : this.formulario.value.logo,
+      actividades : this.formulario.value.actividades,
       usuario: { id: this.idusuario },
       };
 
-      console.log('la experiencia número '+ JSON.stringify(id) + ' fue editada');
+      console.log('la experiencia número '+ this.exp.id + ' fue editada');
 
       this.experiencias.cambiarDato(this.exp).subscribe((data) => { });
 
-      // refresh
-      this.experiencias.obtenerDatos().subscribe((data)=>{
-        this.misexperiencias = data.experiencia;
-      });
+      this.refresh(500);
 
     });
+  }
+  nuevaExperiencia(){
+    this.experienciasmodal = [{
+      empresa : '',
+      puesto : '',
+      fechaInicio : new Date(2022,10,2),
+      fechaFinal : new Date(2022,10,2),
+      logo : '',
+      actividades : '',
+    }]
+    setTimeout(() => {
+      var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+      myModal.show();
+    }, 50);
+
   }
 }

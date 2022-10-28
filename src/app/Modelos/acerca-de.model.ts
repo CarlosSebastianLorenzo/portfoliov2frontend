fc4ac613ce06f4144baeb1761b8549955b362def
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-10-27 22:12:31 -0300

    Componente Acerca De

---
+++ src/app/Modelos/acerca-de.model.ts
@@ -0,0 +1,10 @@
+export interface AcercaDe {
+    "id" : number,
+    "nombre" : string,
+    "color" : string,
+    "titulo" : string,
+    "fotoPerfil" : string,
+    "fotoPortada" : string,
+    "info" : string,
+    "usuario" : { "id" : number }
+}
--- src/app/componentes/acerca-de/acerca-de.component.ts
+++ src/app/componentes/acerca-de/acerca-de.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AcercaDeService } from 'src/app/servicios/acerca-de.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { AcercaDe } from 'src/app/Modelos/acerca-de.model';
+import { AutenticacionService } from '../../servicios/autenticacion.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-acerca-de',
@@ -6,10 +12,108 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit {
+  miacercade:any;
+  public acde: AcercaDe;
+  acercademodal: any;
+  idusuario: number = 0;
+  formulario:FormGroup;
+  isLogged: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private acercaDe: AcercaDeService,
+    private route: ActivatedRoute,
+    private autenticacionService:AutenticacionService) {
+      this.acde ={
+        id: 1,
+        nombre : '',
+        color : '',
+        titulo : '',
+        fotoPerfil : '',
+        fotoPortada : '',
+        info : '',
+        usuario : { id : 1 }
+      };
+      this.formulario=this.formBuilder.group({
+        id:[''],
+        nombre:['',[Validators.required]],
+        color:[''],
+        titulo:[''],
+        fotoPerfil:[''],
+        fotoPortada:[''],
+        info: ['']
+      })
+     }
 
   ngOnInit(): void {
+    this.acercaDe.obtenerDatos().subscribe((data)=>{
+      if(data.acercade[0]!=null){
+        this.miacercade = data.acercade[0];
+
+      document.documentElement.style.setProperty('--color-primario', this.miacercade.color);
+      }
+      else{
+        this.miacercade = {
+          nombre: "Jaime Altozano",
+          titulo: "Musicologo y Youtuber",
+          fotoPerfil: "https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3112172/settings_images/SYeGNqjKScWGJmUth0fA_foto_jaime4.png",
+          fotoPortada: "https://www.amadeusescuelademusica.es/wp-content/uploads/2019/03/El-piano-768x432.jpg",
+          info: "Hago vídeos sobre bandas sonoras, enseño piano en musihacks y me descargo en jaimeafterdark"
+        };
+      }
+    });
+
+    //Verifica si la ruta coincide con el usuario logueado y muestra los botones
+    this.route.paramMap.subscribe((ruta: ParamMap) => {
+      this.idusuario = parseInt(ruta.get('ruta')!);
+      var currentUser=this.autenticacionService.UsuarioAutenticado;
+      if (currentUser && currentUser.id==this.idusuario){
+        this.isLogged=true;
+      }
+    });
+  }
+
+  refresh(milisegundos:number) {
+    setTimeout(() => {
+       this.acercaDe.obtenerDatos().subscribe((data)=>{
+        this.miacercade = data.acercade[0];
+      });
+    }, milisegundos);
+  }
+
+  openModal(acercaDe:AcercaDe){
+    this.acercademodal =[acercaDe];
+    setTimeout(() => {
+      var myModal = new bootstrap.Modal(document.getElementById('acercadeModal'));
+      myModal.show();
+    }, 50);
+  }  
+
+  onChange(event: Event) {
+    event.preventDefault;
+    
+    this.route.paramMap.subscribe((ruta: ParamMap) => {
+      this.idusuario = parseInt(ruta.get('ruta')!);
+
+      this.acde = {
+      id: parseInt(this.formulario.value.id),
+      nombre : this.formulario.value.nombre,
+      color : this.formulario.value.color,
+      titulo : this.formulario.value.titulo,
+      fotoPerfil : this.formulario.value.fotoPerfil,
+      fotoPortada : this.formulario.value.fotoPortada,
+      info : this.formulario.value.info,
+      usuario: { id: this.idusuario },
+      };
+
+      console.log('Acerca De ha sido editado');
+
+      this.acercaDe.cambiarDato(this.acde).subscribe((data) => { });
+      
+      document.documentElement.style.setProperty('--color-primario', this.formulario.value.color);
+
+      this.refresh(500);
+
+    });
   }
 
 }

7c86fee40bc4217bf0b6c27a42ad54fa6246277f
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2022-10-31 01:42:01 -0300

    Enviroment api Url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://cslportfoliov2.herokuapp.com'
+//  apiUrl: 'http://localhost:8080'
+
 };
 
 /*
